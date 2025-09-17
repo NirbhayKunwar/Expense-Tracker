8be11e715bc1ebd7cbfe80cf70f904d6a5ef02c7
nirbhaykunwar19@gmail.com <nirbhaykunwar19@gmail.com>
2025-09-17 20:40:37 +0545

    Fix frontend build path for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ const __dirname1 = path.resolve();
 
 // Serve frontend build files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
   app.use((req, res) =>
-    res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname1, "../frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
